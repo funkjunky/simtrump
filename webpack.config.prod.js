1ddd4c706438f17968c0af236e12826aad597659
Jason McCarrell <jason.abz@gmail.com>
2017-03-22 23:35:53 -0400

    zzz

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: '#eval-source-map',
   entry: [
     './src/index.js'
   ],
@@ -11,21 +11,38 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/static/'
   },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin()
+  ],
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        include: path.resolve(__dirname, 'src'),
-        loader: 'babel-loader',
-        query: {
-            presets: ['es2015', 'react', 'stage-0']
-        }
+        include: path.join(__dirname, 'src'),
+          loader: 'babel-loader',
+          query: {
+              presets: ['es2015', 'stage-0', 'react'].map(dep => require.resolve(`babel-preset-${dep}`)),
+              plugins: [
+                        'transform-runtime',
+              ].map(dep => require.resolve(`babel-plugin-${dep}`)).concat(['react-hot-loader/babel'])
+          }
       },
       {
         test: /\.(png|jpg|wav|mp3)$/,
         loaders: ['url?limit=100000'],
-        include: path.resolve(__dirname, 'assets')
+        include: path.join(__dirname, 'assets')
       },
+
+      // pixi uses fs.readFileSync and require()s json files
+      {
+        test: /\.js$/,
+        loaders: ['transform?brfs'],
+        include: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loaders: ['json']
+      }
     ]
   }
 };
